623885867113d1ec134d67ff945c0602d36ea2a7
Gerald Ooi <27208744+geraldooi@users.noreply.github.com>
2025-07-27 23:20:30 +0800

    fix: hard coded warehouse port

--- include/utils/warehouse.py
+++ include/utils/warehouse.py
@@ -4,5 +4,5 @@ from sqlalchemy import create_engine
 _warehouse = BaseHook.get_connection("warehouse")
 
 engine = create_engine(
-    f"postgresql://{_warehouse.login}:{_warehouse.password}@{_warehouse.host}:5432/{_warehouse.schema}"
+    f"postgresql://{_warehouse.login}:{_warehouse.password}@{_warehouse.host}:{_warehouse.port}/{_warehouse.schema}"
 )
